a432872f597c5e8df780b9701744b099ea85aed1
ShafayetAhmad <shafayetahmad1@gmail.com>
2023-08-11 17:46:05 +0530

    index page updated

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Learning</title>
+</head>
+
+<body>
+    <h1>Welcome to PHP</h1>
+    <?php
+    echo "this is written using php";
+    $num1 = 10;
+    $num2 = 20;
+    echo " ", $num1 + $num2;
+    echo var_dump(1 == 2);
+    ?>
+</body>
+
+</html>
